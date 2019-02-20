749af5c2e00a0bb3ff80b59c51d0434fee594f63
sdass <sdass@drf.com>
2019-02-20 12:10:04 -0500

    list dynamically populated with ajax asynchronous call and a global variable. Todo try synchronous call and use local variable

---
+++ UserQueries/WebFormJQuery.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormJQuery.aspx.cs" Inherits="UserQueries.WebFormJQuery" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>pagejquery</title>
+    <script type="text/javascript" lang="javascript" src="scripts/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" lang="javascript" src="scripts/jquery-ui-1.12.1.min.js"></script>
+    <script type="text/javascript" lang="javascript" src="scripts/custom/fillListboxEtc.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div title="testing">
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:DropDownList ID="DropDownList1" runat="server" ></asp:DropDownList>
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" SelectionMode="Multiple" Height="60px"></asp:ListBox>
+
+        <hr />
+      <%--  path <% Response.Write("aaa=" + HttpContext.Current.Request.Url.AbsolutePath); %>--%>
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ UserQueries/scripts/custom/fillListboxEtc.js
@@ -0,0 +1,62 @@
+﻿
+
+var trackArray = []; //global
+
+$(document).ready(
+    function () {
+        console.log("ready!!");
+
+        //put an ajax call to read the track list from a file or database and fill up the text box eventually.
+        
+        getTracksInjoson(fillUpTrackListBox);
+
+        /*
+        var a = {};
+        a["fullName"] = 'Aqueduct'; a["abbrev"] = 'AQU';
+        trackArray.push(a);
+        //-------------
+        a = {};
+        a["fullName"] = 'Gulfstream'; a["abbrev"] = 'GF';
+        trackArray.push(a);
+        //-----------------
+        a = {};
+        a["fullName"] = 'Laurel'; a["abbrev"] = 'LARC';
+        trackArray.push(a);
+        //-----------------
+        a = {};
+        a["fullName"] = 'Santa Anita'; a["abbrev"] = 'SAN';
+        trackArray.push(a);
+        */
+        //-------fill 2 listboxes below---------
+        var trackDropDnList = $('#DropDownList1');
+        trackDropDnList.append($("<option/>").val('FL,NY,CH').text('EAST'));
+        trackDropDnList.append($("<option/>").val('CA,SC,TO').text('WEST'));
+
+
+
+
+    });
+
+function fillUpTrackListBox() {
+    var track2Listbox = $('#ListBox1')
+    $.each(trackArray, function (index, track) {
+        console.log(index + " ----- " + track.fullName + " : " + track.abbrev);
+        track2Listbox.append($("<option/>").val(track.abbrev).text(track.fullName));
+    });
+}
+
+function getTracksInjoson(callback) {
+    $.ajax({
+        dataType: "json",
+        url: 'http://localhost:50049/data/alltracks.json',
+        data: '',
+        success: function (rcvdata, textStatus, jqXHR) {
+            console.log("call successsful!");
+            trackArray = rcvdata;
+            callback();
+
+        }
+
+
+    })
+}
